4da21bf34e13ec1f1190a970ac2eb220c280520e
renox <planetrenox@protonmail.com>
2024-03-02 05:15:49 -0800

    1.0

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-// Cute logging utilities
+/**
+ * Same as console.log but returns. This is weird behavior.
+ *
+ * @returns {*} - The last argument passed to the function.
+ */
 export const _ = (...args) =>
 {
     console.log(...args);
+    return args[args.length - 1];
 };
-export const __ = (...args) =>
+/**
+ * We won't support anyone who wants to log erro(r) in this manner.
+ *
+ * @param r - <redacted>
+ * @prints <redacted>
+ *
+ */
+export const erro = (r) =>
 {
-    _(`⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀
+    detailedErrorLogger(r);
+};
+
+export const ch = {
+    iiiiiiiiiiii: () =>
+    {
+        return _(`⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀
 ⢸⠉⣹⠋⠉⢉⡟⢩⢋⠋⣽⡻⠭⢽⢉⠯⠭⠭⠭⢽⡍⢹⡍⠙⣯⠉⠉⠉⠉⠉⣿⢫⠉⠉⠉⢉⡟⠉⢿⢹⠉⢉⣉⢿⡝⡉⢩⢿⣻⢍⠉⠉⠩⢹⣟⡏⠉⠹⡉⢻⡍⡇
 ⢸⢠⢹⠀⠀⢸⠁⣼⠀⣼⡝⠀⠀⢸⠘⠀⠀⠀⠀⠈⢿⠀⡟⡄⠹⣣⠀⠀⠐⠀⢸⡘⡄⣤⠀⡼⠁⠀⢺⡘⠉⠀⠀⠀⠫⣪⣌⡌⢳⡻⣦⠀⠀⢃⡽⡼⡀⠀⢣⢸⠸⡇
 ⢸⡸⢸⠀⠀⣿⠀⣇⢠⡿⠀⠀⠀⠸⡇⠀⠀⠀⠀⠀⠘⢇⠸⠘⡀⠻⣇⠀⠀⠄⠀⡇⢣⢛⠀⡇⠀⠀⣸⠇⠀⠀⠀⠀⠀⠘⠄⢻⡀⠻⣻⣧⠀⠀⠃⢧⡇⠀⢸⢸⡇⡇
@@ -17,5 +35,24 @@ export const __ = (...args) =>
 ⢸⠀⡟⠀⢸⡆⢹⡜⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠋⣾⡏⡇⡎⡇⠀⡇
 ⢸⠀⢃⡆⠀⢿⡄⠑⢽⣄⠀⠀⠀⢀⠂⠠⢁⠈⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠄⡐⢀⠂⠀⠀⣠⣮⡟⢹⣯⣸⣱⠁⠀⡇
 ⠈⠉⠉⠉⠉⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁`);
+    }
 };
+
+/**
+ * Logs detailed information about an error object.
+ * @param {Error} error The error object to log.
+ * @param {string} context Optional. A description of where the error occurred.
+ */
+function detailedErrorLogger(error, context = '')
+{
+    console.error(`[Error${context ? ` in ${context}` : ''}]: ${error.message}\nError Name: ${error.name}\nStack Trace: ${error.stack}`);
+
+    // Log custom error properties that are not part of the standard Error object
+    Object.keys(error).forEach(key =>
+    {
+        if (!['name', 'message', 'stack'].includes(key)) {
+            console.log(`Additional Info - ${key}: ${error[key]}`);
+        }
+    });
+}
 
--- src/test.js
+++ src/test.js
@@ -1,8 +1,6 @@
 // import dotenv from 'dotenv'; dotenv.config(); // process.env.EDIT_ME
-import { _, __ } from './index.js';
+import { _ } from './index.js';
 
 const test = () => _({ key: 'value' });
-const test2 = () => __('Verbose log message', { key: 'value' });
 
 test();
-test2();
